ad46875b52dce60a7303a13144fdb587ecb3adc0
PriyanshuKumarMorya <pk2784554@gmail.com>
2026-07-08 23:24:30 +0530

    Add files via upload

---
+++ SUMOFTWONUMBER.java
@@ -0,0 +1,15 @@
+package BE_LEVEL;
+
+import java.util.*;
+
+public class SUMOFTWONUMBER {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int x = sc.nextInt();
+        System.out.print("Enter a number : ");
+        int y = sc.nextInt();
+        System.out.println("Sum of two numbers: "+(x+y));
+
+    }
+}
