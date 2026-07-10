b1a62200b3c6ea4b572085306b85e1d883c4952b
PriyanshuKumarMorya <pk2784554@gmail.com>
2026-07-10 20:59:37 +0530

    Add files via upload

---
+++ SquarePattern.java
@@ -0,0 +1,15 @@
+package PatterQuestion;
+import java.util.*;
+public class SquarePattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n= sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
