d8badadce0ce62c8d5747515598ecd31cd796893
PriyanshuKumarMorya <pk2784554@gmail.com>
2026-07-12 22:26:54 +0530

    Add files via upload

---
+++ LogicalOpertors.java
@@ -0,0 +1,9 @@
+public class LogicalOpertors {
+    public static void main(String[] args){
+        int a=23,b=76;
+//        &&,||,!
+        System.out.println((a>b)&&(a<b));
+        System.out.println((a>b)||(a<b));
+        System.out.println(!(a==b));
+    }
+}
