29c7c6f4387c381a9928da0f8e740b30c9596e0c
PriyanshuKumarMorya <pk2784554@gmail.com>
2026-07-13 22:20:08 +0530

    Add files via upload

---
+++ uniqueNumber.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class uniqueNumber {
+    static int findunique(int arr[]){
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]==arr[j]){
+                    arr[i]=-1;
+                    arr[j]=-1;
+                }
+            }
+        }
+        int ans=-1;
+        for(int i=0;i<n;i++){
+            if(arr[i]>0){
+                ans=arr[i];
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a size of array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.print("Enter " + n + " elements: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(findunique(arr));
+    }
+}
