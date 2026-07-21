365ec33fc93a53b55ec2c9ace9802ca6a01b4fff
PriyanshuKumarMorya <pk2784554@gmail.com>
2026-07-21 16:13:48 +0530

    Add files via upload

---
+++ StaticKeywordUsed.java
@@ -0,0 +1,12 @@
+package Lecture;
+
+class Priyanshu{
+    public static void display(){
+        System.out.println("Welcome to Priyanshu Kumar");
+    }
+}
+public class StaticKeywordUsed {
+    public static void main(String[] args) {
+        Priyanshu.display();
+    }
+}
