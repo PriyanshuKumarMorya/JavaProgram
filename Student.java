2bdcc34b277f1b1fc2e13c5e5137015e4c225496
PriyanshuKumarMorya <pk2784554@gmail.com>
2026-07-22 22:57:50 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,19 @@
+package OOPSinJava;
+
+public class Student {
+    public static class Priyanshu{
+        String name;
+        int rno;
+        double percent;
+    }
+    public static void main(String[] args){
+        Priyanshu x = new Priyanshu();
+        x.name = "Priyanshu";
+        x.rno = 76;
+        x.percent = 9.27;
+        System.out.println(x.percent);
+        System.out.println(x.name);
+        System.out.println(x.rno);
+
+    }
+}
