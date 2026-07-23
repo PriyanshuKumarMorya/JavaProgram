58c8a2a65ad1343a9c9f110936d31ea96a0fcf64
PriyanshuKumarMorya <pk2784554@gmail.com>
2026-07-23 18:48:22 +0530

    Add files via upload

---
+++ Priyanshu8.java
@@ -0,0 +1,15 @@
+package JavaClassProblem;
+
+class Ajay{
+    public final void display(){
+        System.out.println("hello");
+    }
+}
+
+public class Priyanshu8 {
+    public static void main(String[] args) {
+        Ajay A = new Ajay();
+        A.display();
+
+    }
+}
