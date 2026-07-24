87bd3f9b19a7c1e19d60cb4c793b69d05f62deb5
PriyanshuKumarMorya <pk2784554@gmail.com>
2026-07-24 20:24:23 +0530

    Add files via upload

---
+++ Priyanshu10.java
@@ -0,0 +1,16 @@
+package JavaClassProblem;
+
+class College{
+    void name(){
+        System.out.println("Coer University");}}
+class Teachers extends College{
+    void teachername(){
+        System.out.println("Teacher name is :Ajay sir");}}
+class St extends College{
+    void studentname(){
+        System.out.println("Student name is : Priyanshu kumar");}}
+public class Priyanshu10 {
+    public static void main(String[] args) {
+
+    }
+}
