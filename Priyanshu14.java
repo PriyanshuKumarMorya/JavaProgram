881cb87032d098f18621051add849b91861bb27c
PriyanshuKumarMorya <pk2784554@gmail.com>
2026-07-25 21:05:46 +0530

    Add files via upload

---
+++ Priyanshu14.java
@@ -0,0 +1,13 @@
+package JavaClassProblem;
+class Tanu{
+    static void display(){
+        System.out.println("I Love you Priyanshu");
+    }
+}
+
+public class Priyanshu14 {
+    public static void main(String[] args) {
+        Tanu.display();
+
+    }
+}
