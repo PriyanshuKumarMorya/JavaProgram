c1e25dae5daa3e49a525225894c6df2680ad6dce
PriyanshuKumarMorya <pk2784554@gmail.com>
2026-07-31 21:45:48 +0530

    Add files via upload

---
+++ Project1.java
@@ -0,0 +1,15 @@
+package MYSQLPROJECTDATABSE;
+import java.sql.*;
+import java.util.*;
+
+
+public class Project1 {
+    public static void main(String[] args) {
+        try {
+
+        }catch(Exception e){
+            System.out.println(" ");
+
+        }
+    }
+}
