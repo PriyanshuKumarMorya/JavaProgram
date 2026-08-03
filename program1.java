6d6cb9d0c374090a098197e2cd7636e412512ba3
Priyanshu Kumar <pk2784554@gmail.com>
2026-08-03 22:33:50 +0530

    Add files via upload

---
+++ program1.java
@@ -0,0 +1,10 @@
+package String;
+
+public class program1 {
+    public static void main(String[] args) {
+        String  str="Priyanshu kumar";
+        System.out.println(str.indexOf("i"));
+        System.out.println(str.charAt(9));
+
+    }
+}
