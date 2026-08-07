4f86c3180625c7ae36760384cea52300b5e0c61f
Priyanshu Kumar <pk2784554@gmail.com>
2026-08-07 21:55:47 +0530

    Add files via upload

---
+++ Kkumkum.java
@@ -0,0 +1,17 @@
+package Kumkum;
+
+import java.util.Scanner;
+
+public class Kkumkum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+//        for(int i=1 ; i>=0;i++){
+//            System.out.println("Tanu I love You.");
+//        }
+      System.out.println("Tanu I Love you So much .");
+      System.out.println("You will be back my life .");
+      System.out.println("Tanu meri jaan .");
+      System.out.println("Tanu I miss You .");
+
+    }
+}
