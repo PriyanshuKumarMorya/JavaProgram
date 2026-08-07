efb9e26492618bb516c93bb1745e9a742b10cbbd
Priyanshu Kumar <pk2784554@gmail.com>
2026-08-07 21:58:02 +0530

    Add files via upload

---
+++ Tannu4.java
@@ -0,0 +1,16 @@
+package Kumkum;
+
+public class Tannu4 {
+     public final void sound (){
+        System.out.println("Hii");
+    }
+    public void love1 (){
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        Tannu4 t5= new Tannu4();
+        t5.sound();
+
+    }
+}
