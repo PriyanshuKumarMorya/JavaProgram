351f809f55cfed2eacc461ae1fb943c9e99c8ad6
Priyanshu Kumar <pk2784554@gmail.com>
2026-08-07 21:58:27 +0530

    Add files via upload

---
+++ Happy.java
@@ -0,0 +1,11 @@
+package Kumkum;
+
+public class Happy {
+
+        public static void main(String[] args) {
+//            for(int i=1;i>0;i++){
+                System.out.println("WISH WISH VERY VERY HAPPY BIRTHDAY VIRAT BETA ");
+//            }
+        }
+    }
+
