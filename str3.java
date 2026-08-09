61c31712ac8864613a27ce31eb3ed668d0e1d1ab
Priyanshu Kumar <pk2784554@gmail.com>
2026-08-10 01:04:41 +0530

    Add files via upload

---
+++ str3.java
@@ -0,0 +1,12 @@
+package String;
+
+public class str3 {
+    public static void main(String[] args) {
+        String str = "Priyanshu kumar";
+        System.out.println(str.contains("Priy"));
+
+
+        System.out.println(str.startsWith("priyan"));
+
+    }
+}
