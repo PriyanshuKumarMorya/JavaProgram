64da5425770bac6f6e0c4b33f49192f3f62203ae
Priyanshu Kumar <pk2784554@gmail.com>
2026-08-11 00:29:31 +0530

    Add files via upload

---
+++ str5.java
@@ -0,0 +1,10 @@
+package String;
+
+public class str5 {
+    public static void main(String[] args) {
+        String s = "physics";
+        for(int j =2;j<4;j++){
+            System.out.println(s.substring(j));
+        }
+    }
+}
