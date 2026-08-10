ccef48e1b58b384ba8ac0e70c6d7cc951e3c5ba7
Priyanshu Kumar <pk2784554@gmail.com>
2026-08-11 00:29:12 +0530

    Add files via upload

---
+++ str4.java
@@ -0,0 +1,8 @@
+package String;
+
+public class str4 {
+    public static void main(String[] args) {
+        String str = "abcdacdsfgaghajajja";
+        System.out.println(str.substring(4));
+    }
+}
