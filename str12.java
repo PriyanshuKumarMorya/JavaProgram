e94a219c83e5c1aa93bc4365ebe7ca2e15a658dc
Priyanshu Kumar <pk2784554@gmail.com>
2026-08-12 21:02:23 +0530

    Add files via upload

---
+++ str12.java
@@ -0,0 +1,13 @@
+package String;
+
+import java.util.Scanner;
+
+public class str12 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name : ");
+        String str = input.nextLine();
+        System.out.println(str);
+
+    }
+}
