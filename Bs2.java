ffd575e3e4cc437138b92fbed5dc626debdad5f7
Priyanshu Kumar <pk2784554@gmail.com>
2026-08-13 19:25:03 +0530

    Add files via upload

---
+++ Bs2.java
@@ -0,0 +1,10 @@
+package BuilderString;
+
+public class Bs2 {
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder("Priyanshu");
+        System.out.println(str);
+        str.setCharAt(0,'a');
+        System.out.println(str);
+    }
+}
