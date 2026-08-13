1d88bcbe4a42a5b4b34cb942673878adc3634642
Priyanshu Kumar <pk2784554@gmail.com>
2026-08-13 19:25:21 +0530

    Add files via upload

---
+++ Bs3.java
@@ -0,0 +1,9 @@
+package BuilderString;
+
+public class Bs3 {
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder("Priyanshu");
+        str.insert(5,'a');
+        System.out.println(str);
+    }
+}
