99748eefbebab1f6c36d2e3aed9a7035092d72ff
Priyanshu Kumar <pk2784554@gmail.com>
2026-08-14 22:40:15 +0530

    Add files via upload

---
+++ Bs4.java
@@ -0,0 +1,9 @@
+package BuilderString;
+
+public class Bs4 {
+    static void main(String[] args) {
+        StringBuilder str = new StringBuilder("PriyanshuT");
+        str.deleteCharAt(9);
+        System.out.println(str);
+    }
+}
