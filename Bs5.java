79fcf6b3cdf70c888e838750c2fab2c891f871d4
Priyanshu Kumar <pk2784554@gmail.com>
2026-08-14 22:40:33 +0530

    Add files via upload

---
+++ Bs5.java
@@ -0,0 +1,9 @@
+package BuilderString;
+
+public class Bs5 {
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder("Priyanshu");
+        str.reverse();
+        System.out.println(str);
+    }
+}
