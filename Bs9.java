7906139624d24d3e4a40bc59ee0e40a6ffdbef8b
Priyanshu Kumar <pk2784554@gmail.com>
2026-08-15 16:38:00 +0530

    Add files via upload

---
+++ Bs9.java
@@ -0,0 +1,20 @@
+package BuilderString;
+
+public class Bs9 {
+    public static void main(String[] args) {
+        String str = "abcdcba";
+        int i=0;
+        int j=str.length()-1;
+        boolean flag = true;
+        while(i<j){
+            if(str.charAt(i)!= str.charAt(j)){
+                flag = false;
+                break;
+            }
+            i++;
+            j--;
+        }
+        if(flag==true) System.out.println("Palindrome");
+        else System.out.println("Not Palindrome");
+    }
+}
