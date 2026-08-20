78f6a2402cc4527dfcba8bbb89d6d2b32da3e9b5
Priyanshu Kumar <pk2784554@gmail.com>
2026-08-20 20:58:59 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,18 @@
+package Pattern;
+
+import java.util.*;
+
+public class Square {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number of square: ");
+        int n = sc.nextInt();
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
