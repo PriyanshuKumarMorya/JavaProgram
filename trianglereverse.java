5e411cb0cfde99f817cf0f60b841f0e5205bf786
Priyanshu Kumar <pk2784554@gmail.com>
2026-08-20 20:59:36 +0530

    Add files via upload

---
+++ trianglereverse.java
@@ -0,0 +1,17 @@
+package Pattern;
+
+import java.util.*;
+
+public class trianglereverse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number of reverse triangle: ");
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=(n+-i);j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
